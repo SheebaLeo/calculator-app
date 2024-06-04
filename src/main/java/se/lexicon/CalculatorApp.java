cb4b44eda53d821400f536ee9494916db5c634da
Phiomin Gnana Sheeba <jsheeba1893@gmail.com>
2024-06-04 10:00:57 +0200

    Initial commit of calculator app

---
+++ src/main/java/se/lexicon/CalculatorApp.java
@@ -0,0 +1,53 @@
+package se.lexicon;
+import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueCalculation = true;
+        double result = 0;
+
+        while (continueCalculation) {
+            System.out.print("Enter 1st number: ");
+            double number1 = scanner.nextDouble();
+
+            System.out.print("Enter operators + - * /: ");
+            char operator = scanner.next().charAt(0);
+
+            System.out.print("Enter 2nd number: ");
+            double number2 = scanner.nextDouble();
+
+            switch (operator) {
+                case '+':
+                    result = number1 + number2;
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    break;
+                case '/':
+                    if (number2 != 0){
+                        result = number1 / number2;
+                    } else {
+                        System.out.println("""
+                                Division by zero is not possible""");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operators use the given operator");
+                    continue;
+            }
+            System.out.println("Result = " + result);
+            System.out.print("Do you want to perform another calculation? (y/n): ");
+            char choice = scanner.next().charAt(0);
+            continueCalculation = (choice == 'y' || choice == 'Y');
+        }
+
+        System.out.println("Thank you for using the calculator!");
+        scanner.close();
+
+    }
+}
+
