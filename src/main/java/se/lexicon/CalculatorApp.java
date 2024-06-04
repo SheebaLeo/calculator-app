f1a2ca83ef6626bef53e8295f03f190fb2fa8f73
Phiomin Gnana Sheeba <jsheeba1893@gmail.com>
2024-06-04 10:23:00 +0200

    With exception handling

--- src/main/java/se/lexicon/CalculatorApp.java
+++ src/main/java/se/lexicon/CalculatorApp.java
@@ -16,31 +16,35 @@ public class CalculatorApp {
 
             System.out.print("Enter 2nd number: ");
             double number2 = scanner.nextDouble();
-
-            switch (operator) {
-                case '+':
-                    result = number1 + number2;
-                    break;
-                case '-':
-                    result = number1 - number2;
-                    break;
-                case '*':
-                    result = number1 * number2;
-                    break;
-                case '/':
-                    if (number2 != 0){
-                        result = number1 / number2;
-                    } else {
-                        System.out.println("""
-                                Division by zero is not possible""");
-                    }
-                    break;
-                default:
-                    System.out.println("Invalid operators use the given operator");
-                    continue;
+            try {
+                switch (operator) {
+                    case '+':
+                        result = number1 + number2;
+                        break;
+                    case '-':
+                        result = number1 - number2;
+                        break;
+                    case '*':
+                        result = number1 * number2;
+                        break;
+                    case '/':
+                        if (number2 != 0) {
+                            result = number1 / number2;
+                        } else {
+                            System.out.println("""
+                                    Division by zero is not possible""");
+                        }
+                        break;
+                    default:
+                        System.out.println("Invalid operators use the given operator");
+                        continue;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter numeric values.");
+                continue;
             }
-            System.out.println("Result = " + result);
-            System.out.print("Do you want to perform another calculation? (y/n): ");
+                System.out.println("Result = " + result);
+                System.out.print("Do you want to perform another calculation? (y/n): ");
             char choice = scanner.next().charAt(0);
             continueCalculation = (choice == 'y' || choice == 'Y');
         }
